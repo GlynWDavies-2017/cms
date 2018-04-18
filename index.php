50362b3eb1a55dd8d4d00cfb9c6fae2481b68147
glyn.davies <glynwdavies@gmail.com>
2018-04-18 20:10:49 +0200

    Edited index.php

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
     exit;
   }
 
-  echo "Connected to database $dbName";
-
   $sql = "SELECT * FROM article ORDER BY publishedAt";
 
   $results = mysqli_query($connection, $sql);
@@ -22,19 +20,36 @@
     echo mysqli_error($connection);
   } else {
     $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
-    var_dump($articles);
   }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CMS</title>
-</head>
-<body>
-  <h1>CMS</h1>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CMS</title>
+  </head>
+  <body>
+    <header>
+      <h1>CMS</h1>
+    </header>
+    <main>
+      <?php if(empty($articles)):?>
+        <p>No articles found!</p>
+      <?php else:?>
+        <ul>
+          <?php foreach($articles as $article) : ?>
+            <li>
+              <article>
+                <h2><?= $article['title'];?></h2>
+                <p><?= $article['content'];?></p>
+              </article>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      <?php endif;?>
+    </main>
+  </body>
 </html>
